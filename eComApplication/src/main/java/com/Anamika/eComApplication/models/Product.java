d4125b9ecfee009379c5f14be4ef3996d607e17d
Anamika Verma <anuverma0795@gmail.com>
2025-06-30 02:31:21 +0530

    "model created"

---
+++ eComApplication/src/main/java/com/Anamika/eComApplication/models/Product.java
@@ -0,0 +1,16 @@
+package com.Anamika.eComApplication.models;
+
+
+import lombok.Data;
+
+@Data
+public class Product {
+    private Long id;
+    private String title ;
+    private String description;
+    private double price;
+    private String imageUrl;
+    private String category;
+
+
+}
---
+++ eComApplication/src/main/java/com/Anamika/eComApplication/services/BackendApplication.java
@@ -0,0 +1,13 @@
+package com.Anamika.eComApplication.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
+
+}
